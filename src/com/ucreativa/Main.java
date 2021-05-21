497ae7e9500d5c74b56b881a212884bfd10aeef8
Mariano-UC <84363621+Mariano-UC@users.noreply.github.com>
2021-05-20 20:54:22 -0600

    Cree a Mariano

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    Mariano bello = new Mariano();
+    System.out.println("Hola Clase");
     }
 }
---
+++ src/com/ucreativa/Mariano.java
@@ -0,0 +1,8 @@
+package com.ucreativa;
+
+public class Mariano {
+
+    public Mariano(){
+        System.out.println("I am Iron Man");
+    }
+}

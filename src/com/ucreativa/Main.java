9a3631a0a395c22785f3c575b9d5c7fb364fa037
Mariano-UC <84363621+Mariano-UC@users.noreply.github.com>
2021-05-24 20:20:22 -0600

    Creo que ahora si cree a Virginia

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,11 +1,13 @@
 package com.ucreativa;
 
+import com.ucreativa.familia.Virginia;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     Mariano bello = new Mariano();
     System.out.println("Hola Clase");
-    Virginia mom
+    Virginia mom = new Virginia("Buenos Dias");
     }
 }

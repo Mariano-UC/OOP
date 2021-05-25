9a8a5ca758bfe4f042cbaf7d2999fa7b5d768419
Mariano-UC <84363621+Mariano-UC@users.noreply.github.com>
2021-05-24 21:28:29 -0600

    virginia hobby and age

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -7,7 +7,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     Mariano bello = new Mariano();
-    System.out.println("Hola Clase");
+
     Virginia mom = new Virginia("Buenos Dias");
+    mom.setAge();
+
+    int Age = mom.getAge();
+    System.out.println("My Age is " + Age);
+    System.out.println("Hola Clase");
+    System.out.println(mom.getHobby());
     }
 }
--- src/com/ucreativa/familia/Virginia.java
+++ src/com/ucreativa/familia/Virginia.java
@@ -1,7 +1,22 @@
 package com.ucreativa.familia;
 
 public class Virginia {
-    public Virginia(String saludo){
+    private int age;
+    private  String hobby;
 
-        System.out.println(saludo);    }
+    public Virginia(String saludo){
+        this.age = 60;
+        this.hobby = "leer";
+        System.out.println(saludo);
+    }
+    public void setAge(){
+        this.age = this.age + 1;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getHobby(){
+        return hobby;
+    }
 }
+

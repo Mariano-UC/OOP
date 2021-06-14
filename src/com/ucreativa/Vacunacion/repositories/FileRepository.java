8d27272f2c3c2dc1d30a607f426a55cc05a8194c
Mariano-UC <84363621+Mariano-UC@users.noreply.github.com>
2021-06-14 08:48:14 -0600

    tarea 5, mejorada

--- src/com/ucreativa/Vacunacion/repositories/FileRepository.java
+++ src/com/ucreativa/Vacunacion/repositories/FileRepository.java
@@ -2,6 +2,10 @@ package com.ucreativa.Vacunacion.repositories;
 
 import com.ucreativa.Vacunacion.entities.Persona;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -9,6 +13,17 @@ public class FileRepository implements Repository{
     @Override
     public void save(Persona persona, String marca, Date fecha) {
 
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss" );
+
+
+        String text = persona.getName() + " " + marca + " " + format.format(fecha) + "\n";
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("db.txt",true));
+            writer.append(text);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
